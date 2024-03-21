b1ad7f04a8b56122a9e0492e80d2e7370fc043b0
DinhChuong2909 <vudinhchuong2909@gmail.com>
2024-03-21 08:29:17 +0700

    Finish Home page

--- js/homepage.js
+++ js/homepage.js
@@ -68,3 +68,61 @@ const closeAds = () => {
   ads.style.display = "none";
   ads.style.zIndex = "-1";
 }
+
+const changeTab = (index) => {
+  var tab = document.querySelectorAll('.tab');
+  var tabIndex = document.querySelectorAll('.tab-index');
+  if (index === 0) {
+    tab[0].classList.add('currentIndex');
+    tab[1].classList.remove('currentIndex');
+    tabIndex[0].classList.add('tab-active');
+    tabIndex[1].classList.remove('tab-active');
+  } else {
+    tab[0].classList.remove('currentIndex');
+    tab[1].classList.add('currentIndex');
+    tabIndex[0].classList.remove('tab-active');
+    tabIndex[1].classList.add('tab-active');
+  }
+}
+
+function handleMouseOver(e){
+  let currentImgHover = e.getAttribute('data-active')
+  e.setAttribute("src", currentImgHover)
+}
+function handleMouseOut(e){
+  let currentImgDefault = e.getAttribute('data-default')
+  e.setAttribute("src", currentImgDefault)
+}
+
+const changeBlog = (index) => {
+  const blog = document.querySelectorAll('.blog');
+  const hotNews = document.getElementById('hot-news');
+  const magazine = document.getElementById('j-magazine');
+  if (index === 0) {
+    blog[0].classList.add('blog_title-active');
+    blog[1].classList.remove('blog_title-active');
+    hotNews.style.display = "grid";
+    magazine.style.display = "none";
+  }
+  else {
+    blog[0].classList.remove('blog_title-active');
+    blog[1].classList.add('blog_title-active');
+    hotNews.style.display = "none";
+    magazine.style.display = "grid";
+  }
+}
+
+const showDetails = (img) => {
+  const getName = img.getAttribute('data-name-type');
+  const getImg = img.getAttribute('data-image-type');
+  const getContent = img.getAttribute('data-content-type');
+  const getPosition = img.getAttribute('data-position-type');
+  const name = document.getElementById('data_name');
+  const image = document.getElementById('data_image');
+  const content = document.getElementById('data_content');
+  const position = document.getElementById('data_position');
+  name.innerHTML = getName;
+  image.src = getImg;
+  content.innerHTML = getContent;
+  position.innerHTML = getPosition;
+}
